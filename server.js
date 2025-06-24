6b0d376e6561fe0173e8e6b76d40328118c434be
kanavbajaj <kanavbajaj04@gmail.com>
2025-06-24 17:37:50 +0530

    fix: Set absolute path for views directory

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 require('dotenv').config()
 const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortUrl')
@@ -16,6 +17,7 @@ mongoose.connect(mongoUri, {
   serverSelectionTimeoutMS: 5000 
 });
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
